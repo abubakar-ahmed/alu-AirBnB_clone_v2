e2fc49d4f8681aef637b49b0daca69339c910801
abubakar-ahmed <a.ahmed1@alustudent.com>
2024-01-25 05:23:52 -0800

    task

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -61,7 +61,7 @@ class FileStorage:
         except FileNotFoundError:
             pass
 
-         def delete(self, obj=None):
+    def delete(self, obj=None):
         """Deletes obj from __objects if it’s inside"""
         if obj is not None:
             key = obj.__class__.__name__ + '.' + obj.id
